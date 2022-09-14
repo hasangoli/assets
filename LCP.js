4a334357e5969107abd1f00fcded0b31524e2853
Sann <mrhasangoli@gmail.com>
2022-09-14 10:20:03 +0430

    Create LCP.js

---
+++ LCP.js
@@ -0,0 +1,8 @@
+new PerformanceObserver((entryList) => {
+  for (const entry of entryList.getEntries()) {
+    console.log('LCP candidate:', entry.startTime, entry);
+  }
+}).observe({
+  type: 'largest-contentful-paint',
+  buffered: true
+});
